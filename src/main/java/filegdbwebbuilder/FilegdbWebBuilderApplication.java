9a811853d9c360061a2934b3d034b32f2481ee9e
Adam Gutonski <adam.gutonski@locusview.com>
2024-01-25 16:19:26 -0500

    initial commit

---
+++ src/main/java/filegdbwebbuilder/FilegdbWebBuilderApplication.java
@@ -0,0 +1,13 @@
+package filegdbwebbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilegdbWebBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilegdbWebBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/filegdbwebbuilder/FilegdbWebBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package filegdbwebbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilegdbWebBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
