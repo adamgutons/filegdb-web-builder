165018d0972f1ef64907e8a328c05bfad7d4ab65
Adam Gutonski <adam.gutonski@locusview.com>
2024-01-29 12:10:59 -0500

    make name an attribute of template.java, test create file .gdb

--- src/main/java/filegdbwebbuilder/api/FileGDBTemplateGeneratorService.java
+++ src/main/java/filegdbwebbuilder/api/FileGDBTemplateGeneratorService.java
@@ -3,6 +3,7 @@ package filegdbwebbuilder.api;
 import filegdbwebbuilder.entities.FileGDBTemplate;
 import filegdbwebbuilder.entities.FileGDBTemplateResult;
 import lombok.extern.slf4j.Slf4j;
+import org.gdal.ogr.DataSource;
 import org.gdal.ogr.ogr;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +11,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class FileGDBTemplateGeneratorService {
 
+    private static final String GDB_EXTENSION = ".gdb";
+    private static final String FILE_DRIVER = "OpenFileGDB";
+
     public FileGDBTemplateResult generateFileGDBTemplate(final FileGDBTemplate fileGDBTemplateConfiguration) {
         ogr.RegisterAll();
         log.info("Register GDAL complete");
+        final String templateName = fileGDBTemplateConfiguration.getTemplateName() + GDB_EXTENSION;
+        final DataSource fileGDBtemplateDataSource =
+                ogr.GetDriverByName(FILE_DRIVER).CreateDataSource(templateName);
+        fileGDBtemplateDataSource.delete();
         return FileGDBTemplateResult.builder().build();
     }
 
