e353f440861827b973601c91b83eca2feb146b66
Adam Gutonski <adam.gutonski@locusview.com>
2024-01-29 22:07:23 -0500

    init field logic

--- src/main/java/filegdbwebbuilder/api/FileGDBTemplateGeneratorService.java
+++ src/main/java/filegdbwebbuilder/api/FileGDBTemplateGeneratorService.java
@@ -1,10 +1,13 @@
 package filegdbwebbuilder.api;
 
 import filegdbwebbuilder.api.exception.FileGDBTemplateServiceException;
+import filegdbwebbuilder.entities.FeatureLayer;
 import filegdbwebbuilder.entities.FileGDBTemplate;
 import filegdbwebbuilder.entities.FileGDBTemplateResult;
 import lombok.extern.slf4j.Slf4j;
 import org.gdal.ogr.DataSource;
+import org.gdal.ogr.FieldDefn;
+import org.gdal.ogr.Layer;
 import org.gdal.ogr.ogr;
 import org.gdal.osr.SpatialReference;
 import org.springframework.stereotype.Service;
@@ -30,6 +33,8 @@ public class FileGDBTemplateGeneratorService {
         log.info("Data source created...");
         createOgrLayerObjects(fileGDBTemplateConfiguration, fileTemplateDataSource, createSpatialReference(fileGDBTemplateConfiguration));
         log.info("Layer objects created...");
+        createOgrFieldObjects(fileGDBTemplateConfiguration, fileTemplateDataSource);
+        log.info("Field objects created...");
         fileTemplateDataSource.delete();
 
         return FileGDBTemplateResult.builder().build();
@@ -55,6 +60,28 @@ public class FileGDBTemplateGeneratorService {
                     fileTemplateDataSource.CreateLayer(layerField.getName(), spatialReference, layerField.getGeometryType()));
         } catch (Exception e) {
             throw new FileGDBTemplateServiceException("Error when creating ogr layer objects", e);
+        }
+    }
+
+    private void createOgrFieldObjects(final FileGDBTemplate fileGDBTemplateConfiguration,
+                                       final DataSource fileTemplateDataSource) {
+        try {
+            fileGDBTemplateConfiguration.getFeatureLayers()
+                    .forEach(featureLayerConfig -> createOgrField(featureLayerConfig, fileTemplateDataSource));
+        } catch (Exception e) {
+            throw new FileGDBTemplateServiceException("Error when creating ogr layer objects", e);
+        }
+    }
+
+    private void createOgrField(final FeatureLayer featureLayerConfiguration,
+                                final DataSource fileTemplateDataSource) {
+        try {
+            final Layer ogrLayer = fileTemplateDataSource.GetLayer(featureLayerConfiguration.getName());
+            featureLayerConfiguration.getLayerFields()
+                    .forEach(layerFieldConfig ->
+                            ogrLayer.CreateField(new FieldDefn(layerFieldConfig.getName(), layerFieldConfig.getType())));
+        } catch (Exception e) {
+            throw new FileGDBTemplateServiceException("Error when creating ogr field objects", e);
         }
     }
 
